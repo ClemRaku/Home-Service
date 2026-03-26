51ba369c73a8356a7314fc030647a3014d9e113a
Clement Raka De Costa <clement1raka@gmail.com>
2026-03-27 00:13:31 +0600

    made the height and width 1920 by 1080p

--- homeService/src/App.tsx
+++ homeService/src/App.tsx
@@ -3,7 +3,14 @@ function App() {
     <iframe
       title="Home Service"
       src="/home-service/Html/Home.html"
-      style={{ width: '100%', height: '100vh', border: '0', display: 'block' }}
+      style={{
+        width: '1920px',
+        maxWidth: '100%',
+        height: '1080px',
+        border: '0',
+        display: 'block',
+        margin: '0 auto',
+      }}
     />
   )
 }
